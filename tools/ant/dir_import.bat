af551c6ce2e2fcea6644c12ffd8275d8fcb83ad3
Orkan <orkans@gmail.com>
2021-03-17 15:13:31 +0100

    Tools: add confirmation to dir_import.bat

--- tools/ant/dir_import.bat
+++ tools/ant/dir_import.bat
@@ -1,21 +1,33 @@
 @echo off
 setlocal
 
-REM Config: -------------------------------------------------
+REM Config: --------------------------------------------
 set BUILDFILE=%~dpn0.xml
 set SOURCE=%1
 set TARGET=%2
 set EXTRAS=%3
+set YESNO=%~4
 
-REM Commands: -------------------------------------------------
+REM Confirm: -------------------------------------------
+if "%YESNO%" NEQ "" (
+	set /p ANSWER=%YESNO% [y/N]: 
+)
+if "%YESNO%" NEQ "" (
+	if "%ANSWER%" NEQ "y" (
+		goto :end
+	)
+)
+
+REM Command: -------------------------------------------
 set COMMAND=ant -DSourceDir = %SOURCE% -DTargetDir = %TARGET% -f %BUILDFILE%
-REM -----------------------------------------------------------
 
+REM Run: -----------------------------------------------
 echo.
 cmd /c %COMMAND%
 
-REM -----------------------------------------------------------
-if "nowait" == "%EXTRAS%" (
+REM ----------------------------------------------------
+:end
+if "%EXTRAS%" == "nowait" (
 	exit /b
 ) else (
 	pause
