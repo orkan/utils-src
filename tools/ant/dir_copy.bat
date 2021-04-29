ed2a2ac450ab788b160b4f28b9f4d3062462ee35
Orkan <orkans@gmail.com>
2021-04-29 18:50:01 +0200

    Tools: add ant/dir_copy
    This is a simpler version of dir_import without default excludes. With
    only user includes being copied

---
+++ tools/ant/dir_copy.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+
+REM Config: --------------------------------------------
+set SOURCE=%~1
+set TARGET=%~2
+set INCLUDES=%~3
+set EXTRAS=%~4
+set CONTINUE=%~5
+
+REM Confirm: -------------------------------------------
+if "%CONTINUE%" NEQ "" (
+	set /p ANSWER=%CONTINUE% [y/N]:
+)
+if "%CONTINUE%" NEQ "" (
+	if "%ANSWER%" NEQ "y" (
+		goto :end
+	)
+)
+
+REM Command: -------------------------------------------
+set COMMAND=ant -DSourceDir="%SOURCE%" -DTargetDir="%TARGET%" -DIncludes="%INCLUDES%" -f "%~dpn0.xml"
+
+REM Run: -----------------------------------------------
+echo.
+cmd /c %COMMAND%
+
+REM ----------------------------------------------------
+:end
+if "%EXTRAS%" == "nowait" (
+	exit /b
+) else (
+	pause
+)
